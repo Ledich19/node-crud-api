602e68dff979795e09d5f4bb27259ee9d876821c
Aleksandr <ledich19@gmail.com>
2023-06-23 19:35:21 +0300

    feat: create server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import { logger } from "./modules/helpers.js";
+import "dotenv/config";
+import http from "node:http";
 
-const message = "hello world";
-logger(message);
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World");
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port \x1b[36m ${PORT}\x1b[0m`);
+});
