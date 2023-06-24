8b56d46c59ca96eb040c22af50a0c11880d03657
Aleksandr <ledich19@gmail.com>
2023-06-24 17:20:03 +0300

    refactor: error hendlers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,135 +4,103 @@ import { ReqUserType } from "../app/types.js";
 import { getDataFromRequest, isUUID } from "../utils/healper.js";
 
 const getUsers = async (req: IncomingMessage, res: ServerResponse) => {
-  try {
-    const users = await userModel.getAll();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
-  } catch (error) {
-    if (error instanceof Error) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: error.message }));
-    }
-  }
+  const users = await userModel.getAll();
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(users));
 };
 
 const getUserById = async (req: IncomingMessage, res: ServerResponse) => {
   const id = req.url?.split("/")[3];
-  try {
-    const user = await userModel.getById(id || "");
-    if (!user) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "user does not exist" }));
-      return;
-    }
-    if (id && !isUUID(id)) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "user not valid" }));
-      return;
-    }
-
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(user));
-  } catch (error) {
-    console.error(error);
+  const user = await userModel.getById(id || "");
+  if (!user) {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "user does not exist" }));
+    return;
   }
+  if (id && !isUUID(id)) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "user not valid" }));
+    return;
+  }
+
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(user));
 };
 
 const createUser = async (req: IncomingMessage, res: ServerResponse) => {
-  try {
-    const body = await getDataFromRequest(req);
+  const body = await getDataFromRequest(req);
 
-    if (typeof body !== "string" || typeof body === "undefined") {
-      throw new Error("Invalid request body");
-    }
-
-    const bodyData = JSON.parse(body) as ReqUserType;
-
-    if (
-      typeof bodyData.username !== "string" ||
-      typeof bodyData.age !== "number" ||
-      !Array.isArray(bodyData.hobbies)
-    ) {
-      throw new Error("Invalid request body");
-    }
-
-    const newUser = {
-      username: bodyData.username,
-      age: bodyData.age,
-      hobbies: bodyData.hobbies,
-    };
-
-    console.log("POST", newUser);
-
-    const createdUser = await userModel.create(newUser);
-
-    res.writeHead(201, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(createdUser));
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: error.message }));
-    }
+  if (typeof body !== "string" || typeof body === "undefined") {
+    throw new Error("Invalid request body");
   }
+
+  const bodyData = JSON.parse(body) as ReqUserType;
+
+  if (
+    typeof bodyData.username !== "string" ||
+    typeof bodyData.age !== "number" ||
+    !Array.isArray(bodyData.hobbies)
+  ) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Invalid request body" }));
+  }
+
+  const newUser = {
+    username: bodyData.username,
+    age: bodyData.age,
+    hobbies: bodyData.hobbies,
+  };
+
+  console.log("POST", newUser);
+
+  const createdUser = await userModel.create(newUser);
+
+  res.writeHead(201, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(createdUser));
 };
 
 const updateUserById = async (req: IncomingMessage, res: ServerResponse) => {
-  try {
-    const id = req.url?.split("/")[3];
-    const body = await getDataFromRequest(req);
-    const user = await userModel.getById(id || "");
-    if (typeof body !== "string") {
-      throw new Error("Invalid request body");
-    }
-
-    if (!user) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "user does not exist" }));
-      return;
-    }
-    if (id && !isUUID(id)) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "user not valid" }));
-      return;
-    }
-
-    const bodyData = JSON.parse(body) as ReqUserType;
-    const updatedUser = await userModel.update(id || "", bodyData);
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(updatedUser));
-  } catch (error) {
-    if (error instanceof Error) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: error.message }));
-    }
+  const id = req.url?.split("/")[3];
+  const body = await getDataFromRequest(req);
+  const user = await userModel.getById(id || "");
+  if (typeof body !== "string") {
+    throw new Error("Invalid request body");
   }
+
+  if (!user) {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "user does not exist" }));
+    return;
+  }
+  if (id && !isUUID(id)) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "user not valid" }));
+    return;
+  }
+
+  const bodyData = JSON.parse(body) as ReqUserType;
+  const updatedUser = await userModel.update(id || "", bodyData);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(updatedUser));
 };
 
 const deleteUserById = async (req: IncomingMessage, res: ServerResponse) => {
-  try {
-    const id = req.url?.split("/")[3];
-    const user = await userModel.getById(id || "");
+  const id = req.url?.split("/")[3];
+  const user = await userModel.getById(id || "");
 
-    if (!user) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "user does not exist" }));
-      return;
-    }
-    if (id && !isUUID(id)) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "user not valid" }));
-      return;
-    }
-
-    res.writeHead(204, { "Content-Type": "application/json" });
-    res.end();
-  } catch (error) {
-    if (error instanceof Error) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: error.message }));
-    }
+  if (!user) {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "user does not exist" }));
+    return;
   }
+  if (id && !isUUID(id)) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "user not valid" }));
+    return;
+  }
+
+  res.writeHead(204, { "Content-Type": "application/json" });
+  res.end();
 };
 
 const usersController = (req: IncomingMessage, res: ServerResponse) => {
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,21 @@ import usersController from "./controllers/userController.js";
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+try {
   if (url?.match(/\/api\/users/)) {
     usersController(req, res);
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: `Sorry, page does not exist` }));
   }
+  
+} catch (error) {
+  if (error instanceof Error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: error.message }));
+  }
+}
+
 });
 
 const PORT = process.env.PORT || 3000;
